74c1de8967674e6df262cc324643a4bd864b0b8b
PrajwalPohane <prajwal.22210207@viit.ac.in>
2025-07-19 22:09:33 +0530

    Removed search bar from TopNav

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -23,17 +23,6 @@ const TopNav = ({ onMenuClick }) => {
           >
             <Menu className="h-5 w-5" />
           </button>
-          
-          <div className="hidden sm:block">
-            <div className="relative">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Search..."
-                className="pl-10 pr-4 py-2 bg-slate-700 border border-slate-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-white placeholder-gray-400"
-              />
-            </div>
-          </div>
         </div>
 
         <div className="flex items-center space-x-4">
